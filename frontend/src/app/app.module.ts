41bc6eff142ae1f658d7e8421556de528a02be2b
Edgar7777 <losthedgar@hotmail.com>
2018-08-19 21:44:20 -0500

    rutas y pagina home

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -8,13 +8,15 @@ import { AppComponent } from './app.component';
 import { PageInicioComponent } from './page-inicio/page-inicio.component';
 import { PageLoginComponent } from './page-login/page-login.component';
 import { PageRegistroComponent } from './page-registro/page-registro.component';
+import { PageHomeComponent } from './page-home/page-home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PageInicioComponent,
     PageLoginComponent,
-    PageRegistroComponent
+    PageRegistroComponent,
+    PageHomeComponent
   ],
   imports: [
     BrowserModule,
--- frontend/src/app/rutas/app.routes.ts
+++ frontend/src/app/rutas/app.routes.ts
@@ -3,10 +3,28 @@ import { RouterModule, Routes } from "@angular/router";
 import { PageInicioComponent } from '../page-inicio/page-inicio.component';
 import { PageLoginComponent } from '../page-login/page-login.component';
 import { PageRegistroComponent } from '../page-registro/page-registro.component';
+import { PageHomeComponent } from '../page-home/page-home.component';
 
 export const AppRoutes: Routes=[
   {
+    path:'home',
+    component: PageHomeComponent
+  },
+  {
+    path:'login',
+    component: PageLoginComponent
+  },
+  {
+    path:'registro',
+    component: PageRegistroComponent
+  },
+  {
     path:'inicio',
     component: PageInicioComponent
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
   },
 ]
